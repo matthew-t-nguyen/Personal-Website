7e5928205f8afc6758e03f79acc57b3cb7777642
Matthew Nguyen <matthew.t.nguyen@berkeley.edu>
2023-05-24 19:47:19 -0700

    Add Swiper API css and js files

--- js/main.js
+++ js/main.js
@@ -100,6 +100,18 @@ modalCloses.forEach((modalClose) => {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio-container', {
+    cssMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,    
+});
 
 
 /*==================== TESTIMONIAL ====================*/
